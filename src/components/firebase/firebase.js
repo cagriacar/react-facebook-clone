dbeb98cdef1e70aaf3acb529b09196a650c48600
Cagri Acar <cagriacar22@gmail.com>
2020-10-26 07:19:46 +0300

    Update firebase.js

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyADzuYmSSmFDsiDm7R8-5NvfCjOZC6Ocsk",
-  authDomain: "facebook-clone-42f63.firebaseapp.com",
-  databaseURL: "https://facebook-clone-42f63.firebaseio.com",
-  projectId: "facebook-clone-42f63",
-  storageBucket: "facebook-clone-42f63.appspot.com",
-  messagingSenderId: "398251628286",
-  appId: "1:398251628286:web:f3f010625c9a763561f7af",
-  measurementId: "G-XJNB73JFZD"
+  
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
